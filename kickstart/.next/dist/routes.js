e779088a7ab917b5c84ff911e2ca14ef90a2eee9
Tianhong Yu <hugepeak@gmail.com>
2018-08-12 23:25:13 -0500

    delete build folders

--- kickstart/.next/dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var routes = require("next-routes")();
-
-routes.add("/campaigns/new", "/campaigns/new").add("/campaigns/:address", "/campaigns/show") // : wild card
-.add("/campaigns/:address/requests", "/campaigns/requests/index").add("/campaigns/:address/requests/new", "/campaigns/requests/new");
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNHLEFBREgsSUFDTyxBQURQLGtCQUN5QixBQUR6QixrQkFFRyxBQUZILElBRU8sQUFGUCx1QkFFOEIsQUFGOUIsbUJBRWlELEFBRmpEO0NBR0csQUFISCxJQUdPLEFBSFAsZ0NBR3VDLEFBSHZDLDZCQUlHLEFBSkgsSUFJTyxBQUpQLG9DQUkyQyxBQUozQzs7QUFNQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL1RpYW5ob25nIFl1L0RvY3VtZW50cy9jb2RpbmcvYmxvY2tjaGFpbl9kZW1vL2tpY2tzdGFydCJ9
